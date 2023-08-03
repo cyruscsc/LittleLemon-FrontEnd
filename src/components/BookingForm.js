7a0a9fcad3abec28e1a5495c5832999717c65f7d
Cyrus Chan <sairuscsc@gmail.com>
2023-08-02 23:53:15 -0400

    Work on booking form component

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
 const BookingForm = props => {
-  
+  const [availableDates, setAvailableDates] = React.useState(['date1', 'date2', 'date3']);
+  const [availableTime, setAvailableTime] = React.useState(['time1', 'time2', 'time3']);
+  const [occasions, setOccasions] = React.useState(['Daily', 'Birthday', 'Engagement', 'Anniversary']);
+  const dateRef = React.useRef(null);
+  const timeRef = React.useRef(null);
+  const dinersRef = React.useRef(null);
+  const occasionRef = React.useRef(null);
+  return (
+    <section id='bookingform'>
+      <div className='super-container'>
+        <form>
+          {/* Todo: Create a controlled form */}
+          <select name='date' id='date'>
+            {/* Todo: Create list components */}
+          </select>
+          <select name='time' id='time'>
+            {/* Todo: Create list components */}
+          </select>
+          <select name='diners' id='diners'>
+            {/* Todo: Create list components */}
+          </select>
+          <button type='submit'>Book now</button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default BookingForm;

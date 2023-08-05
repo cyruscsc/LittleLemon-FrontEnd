393114ad2447d1f0852e9ba7ea40ee4070a95e07
Cyrus Chan <sairuscsc@gmail.com>
2023-08-05 13:07:57 -0400

    Finish booking form component

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -2,31 +2,50 @@ import React from 'react'
 import './BookingForm.css'
 
 const BookingForm = props => {
-  const [date, setDate] = React.useState("");
-  const [time, setTime] = React.useState("");
-  const [numDiners, setNumDiners] = React.useState("");
+  const defaultInputState = {value: '', isTouch: false};
+  const [date, setDate] = React.useState(defaultInputState);
+  const [time, setTime] = React.useState(defaultInputState);
+  const [numDiners, setNumDiners] = React.useState(defaultInputState);
   const [occasion, setOccasion] = React.useState("");
   const [seatOption, setSeatOption] = React.useState("");
-  const [firstName, setFirstName] = React.useState("");
-  const [lastName, setLastName] = React.useState("");
-  const [email, setEmail] = React.useState({value: '', isTouch: false});
+  const [firstName, setFirstName] = React.useState(defaultInputState);
+  const [lastName, setLastName] = React.useState(defaultInputState);
+  const [email, setEmail] = React.useState(defaultInputState);
   const [specialRequest, setSpecialRequest] = React.useState("");
+  const [isSuccessful, setIsSuccessful] = React.useState(false);
+  const [isFailed, setIsFailed] = React.useState(false);
 
   const isEmail = emailAddress => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(emailAddress);
-
-  const formIsValid = () => {
-    return (
-      date && time && numDiners && occasion && seatOption &&
-      firstName && lastName && isEmail(email.value)
-    );
+  const formIsValid = () => date && time && numDiners && firstName && lastName && isEmail(email.value);
+  const clearForm = () => {
+    setDate(defaultInputState);
+    setTime(defaultInputState);
+    setNumDiners(defaultInputState);
+    setOccasion("");
+    setSeatOption("");
+    setFirstName(defaultInputState);
+    setLastName(defaultInputState);
+    setEmail(defaultInputState);
+    setSpecialRequest("");
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    setIsSuccessful(false);
+    setIsFailed(false);
+    Math.floor(Math.random() * 2) ? setIsSuccessful(true) : setIsFailed(true);
+    if (isSuccessful) {clearForm()};
   };
 
   return (
     <section id='bookingform'>
       <div className='super-container'>
-        <form className='form-container'>
+        <form onSubmit={handleSubmit} className='form-container'>
           <div className='input-container'>
-            <select name='date' value={date} onChange={e => {setDate(e.target.value);}} id='date' className={`input-box ${date ? 'not-empty' : null }`} required>
+            <select name='date' value={date.value}
+              onChange={e => {setDate({...date, value: e.target.value});}}
+              onBlur={() => setDate({...date, isTouch: true})} id='date'
+              className={`input-box ${date.value ? 'not-empty' : null } ${!date.value && date.isTouch ? 'required' : null}`}
+            >
               <option value=''></option>
               <option value='value 1'>Value 1</option>
               <option value='value 2'>Value 2</option>
@@ -35,7 +54,11 @@ const BookingForm = props => {
             <label htmlFor='date'>Date<span className='required-text'> - required</span></label>
           </div>
           <div className='input-container'>
-            <select name='time' value={time} onChange={e => {setTime(e.target.value);}} id='time' className={`input-box ${time ? 'not-empty' : null }`} required>
+            <select name='time' value={time.value}
+              onChange={e => {setTime({...time, value: e.target.value});}}
+              onBlur={() => setTime({...time, isTouch: true})} id='time'
+              className={`input-box ${time.value ? 'not-empty' : null } ${!time.value && time.isTouch ? 'required' : null}`}
+            >
               <option value=''></option>
               <option value='value 1'>Value 1</option>
               <option value='value 2'>Value 2</option>
@@ -44,25 +67,32 @@ const BookingForm = props => {
             <label htmlFor='time'>Time<span className='required-text'> - required</span></label>
           </div>
           <div className='input-container'>
-            <select name='numDiners' value={numDiners} onChange={e => {setNumDiners(e.target.value);}} id='numDiners' className={`input-box ${numDiners ? 'not-empty' : null }`} required>
+            <select name='numDiners' value={numDiners.value}
+              onChange={e => {setNumDiners({...numDiners, value: e.target.value});}}
+              onBlur={() => setNumDiners({...numDiners, isTouch: true})} id='numDiners'
+              className={`input-box ${numDiners.value ? 'not-empty' : null } ${!numDiners.value && numDiners.isTouch ? 'required' : null}`}
+            >
               <option value=''></option>
-              <option value='value 1'>Value 1</option>
-              <option value='value 2'>Value 2</option>
-              <option value='value 3'>Value 3</option>
+              <option value='value 1'>1-2</option>
+              <option value='value 2'>3-4</option>
+              <option value='value 3'>5-6</option>
             </select>
             <label htmlFor='numDiners'>Number of Diners<span className='required-text'> - required</span></label>
           </div>
           <div className='input-container'>
-            <select name='occasion' value={occasion} onChange={e => {setOccasion(e.target.value);}} id='occasion' className={`input-box ${occasion ? 'not-empty' : null }`} required>
+            <select name='occasion' value={occasion}
+              onChange={e => {setOccasion(e.target.value);}}
+              className={`input-box ${occasion ? 'not-empty' : null }`}
+            >
               <option value=''></option>
-              <option value='value 1'>Value 1</option>
-              <option value='value 2'>Value 2</option>
-              <option value='value 3'>Value 3</option>
+              <option value='value 1'>Birthday</option>
+              <option value='value 2'>Engagement</option>
+              <option value='value 3'>Anniversary</option>
             </select>
-            <label htmlFor='occasion'>Occasion<span className='required-text'> - required</span></label>
+            <label htmlFor='occasion'>Occasion (optional)</label>
           </div>
           <div className='input-container seating-container'>
-            <legend>Seating Options<span className='required-text'> - required</span></legend>
+            <legend className={`${seatOption ? 'not-empty' : null }`}>Seating Options (optional)</legend>
             <div className='seating-option'>
               <input type='radio' name='standard' value='standard' onChange={e => {setSeatOption(e.target.value);}} checked={seatOption==='standard' ? true : false} id='standard' />
               <label htmlFor='standard'>Standard</label>
@@ -73,24 +103,38 @@ const BookingForm = props => {
             </div>
           </div>
           <div className='input-container'>
-            <input type='text' name='firstName' value={firstName} onChange={e => {setFirstName(e.target.value);}} id='firstName' className={`input-box ${firstName ? 'not-empty' : null }`} required />
+            <input type='text' name='firstName' value={firstName.value}
+              onChange={e => {setFirstName({...firstName, value: e.target.value});}}
+              onBlur={() => setFirstName({...firstName, isTouch: true})} id='firstName'
+              className={`input-box ${firstName.value ? 'not-empty' : null } ${!firstName.value && firstName.isTouch ? 'required' : null}`}
+            />
             <label htmlFor='firstName'>First Name<span className='required-text'> - required</span></label>
           </div>
           <div className='input-container'>
-            <input type='text' name='lastName' value={lastName} onChange={e => {setLastName(e.target.value);}} id='lastName' className={`input-box ${lastName ? 'not-empty' : null }`} required />
+            <input type='text' name='lastname' value={lastName.value}
+              onChange={e => {setLastName({...lastName, value: e.target.value});}}
+              onBlur={() => setLastName({...lastName, isTouch: true})} id='lastname'
+              className={`input-box ${lastName.value ? 'not-empty' : null } ${!lastName.value && lastName.isTouch ? 'required' : null}`}
+            />
             <label htmlFor='lastName'>Last Name<span className='required-text'> - required</span></label>
           </div>
           <div className='input-container'>
-            <input type='email' name='email' value={email.value} onChange={e => {setEmail({...email, value: e.target.value});}} onBlur={() => setEmail({...email, isTouch: true})} id='email' className={`input-box ${email.value ? 'not-empty' : null } ${!email.value && email.isTouch ? 'required' : null} ${!isEmail(email.value) && email.isTouch ? 'invalid' : null}`} required />
+            <input type='email' name='email' value={email.value}
+              onChange={e => {setEmail({...email, value: e.target.value});}}
+              onBlur={() => setEmail({...email, isTouch: true})} id='email' 
+              className={`input-box ${email.value ? 'not-empty' : null } ${!email.value && email.isTouch ? 'required' : null} ${!isEmail(email.value) && email.isTouch ? 'invalid' : null}`}
+            />
             <label htmlFor='email'>Email<span className='required-text'> - required</span><span className='invalid-text'> - invalid</span></label>
           </div>
           <div className='input-container'>
-            <input type='text' name='specialRequest' value={specialRequest} onChange={e => {setSpecialRequest(e.target.value);}} id='specialRequest' className={`input-box ${specialRequest ? 'not-empty' : null }`} required />
-            <label htmlFor='specialRequest'>Special Request</label>
+            <input type='text' name='specialRequest' value={specialRequest} onChange={e => {setSpecialRequest(e.target.value);}} id='specialRequest' className={`input-box ${specialRequest ? 'not-empty' : null }`} />
+            <label htmlFor='specialRequest'>Special Request (optional)</label>
           </div>
           <div className='input-container'>
             <button type='submit' className='button' disabled={!formIsValid()}>Book now</button>
           </div>
+          {isSuccessful && <div className='status-block successful'><p>Your booking is confirmed!</p></div>}
+          {isFailed && <div className='status-block failed'><p>Your booking is not successful, please try again!</p></div>}
         </form>
       </div>
     </section>
